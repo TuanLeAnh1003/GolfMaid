9c7e6b7e0e4480bf238926b9e46076b8e061cae8
TuanLeAnh1003 <nhoxtboy1000@gmail.com>
2022-05-06 16:02:00 +0700

    connect DB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const URI = 'mongodb+srv://leanhtuan:leanhtuan@cluster0.8svsf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-});
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    });
+  }).catch((err) => {
+    console.log('err', err);
+  });
+
