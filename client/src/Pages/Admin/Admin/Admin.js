0da83ba8bcd78356eacbb01732474ead43d2009f
phokhanhhung <phokhanhhung2409@gmail.com>
2022-05-06 17:11:00 +0700

    delete files

--- client/src/Pages/Admin/Admin/Admin.js
+++ client/src/Pages/Admin/Admin/Admin.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import './Admin.css';
-import {Routes, Route} from 'react-router-dom';
-
-import Statistic from '../Statistic/Statistic';
-import SettingOptions from '../SettingOptions/SettingOptions';
+import { Route, Routes } from 'react-router-dom';
 import AdminProfile from '../AdminProfile/AdminProfile';
-import OrderManagement from '../OrderManagement/OrderManagement';
-import ResponseManagement from '../ResponseManagement/ResponseManagement';
-import AdminHeader from './../../../Components/Header/AdminHeader';
-import AdminSideBar from './../../../Components/AdminSideBar/AdminSideBar';
-import AdminFooter from './../../../Components/Footer/AdminFooter';
-import UserManagement from '../UserManagement';
-import UserCreate from '../UserManagement/UserCreate';
-import UserUpdate from '../UserManagement/UserUpdate';
-import OrderView from '../OrderView';
 import NewsManagement from '../NewsManagement';
 import NewsCreate from '../NewsManagement/NewsCreate';
 import NewsUpdate from '../NewsManagement/NewsUpdate';
-import CustomerManagement from '../CustomerManagement/CustomerManagement';
-import ProductManagement from '../ProductManagement/ProductManagement';
+import SettingOptions from '../SettingOptions/SettingOptions';
+import UserManagement from '../UserManagement';
+import UserCreate from '../UserManagement/UserCreate';
+import UserUpdate from '../UserManagement/UserUpdate';
+import AdminSideBar from './../../../Components/AdminSideBar/AdminSideBar';
+import AdminFooter from './../../../Components/Footer/AdminFooter';
+import AdminHeader from './../../../Components/Header/AdminHeader';
+import './Admin.css';
+
 
 function Admin() {
   return (
@@ -29,20 +23,12 @@ function Admin() {
         <div className='admin__col'>
           <div>
             <Routes>
-              <Route path='statistic' element={<Statistic />} />
               <Route path='settings' element={<SettingOptions />} />
               <Route path='admin-profile' element={<AdminProfile />} />
-              <Route path='order-management' element={<OrderManagement />} />
               <Route path='user-management' element={<UserManagement />} />
-              <Route path='customer-management' element={<CustomerManagement />} />
-              <Route path='product-management' element={<ProductManagement />} />
               <Route path='user-create' element={<UserCreate />} />
               <Route path='user-update' element={<UserUpdate />} />
-              <Route
-                path='response-management'
-                element={<ResponseManagement />}
-              />
-              <Route path='order-view' element={<OrderView />} />
+              
               <Route path='news-management' element={<NewsManagement />} />
               <Route path='news-create' element={<NewsCreate />} />
               <Route path='news-update' element={<NewsUpdate />} />
--- client/src/Pages/HomePage/HomePage/HomePage.js
+++ client/src/Pages/HomePage/HomePage/HomePage.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Footer from '../../../Components/Footer/Footer';
 import Header from '../../../Components/Header/Header';
-import Home from '../Home';
-import ProductDetail from '../ProductDetail';
-import SearchOrder from '../SearchOrder'
-import ResultSearchOrder from '../ResultSearchOrder'
-import News from '../News/News'
-import WatchNews from '../WatchNews';
-import Contact from '../Contact';
-import Cash from '../Payment/Cash';
-import Momo from '../Payment/Momo';
-import Search from '../Search';
-import SaleOff from '../SaleOff';
-import Payment from '../Payment/Payment';
-import Cart from '../Cart';
-import LikedProduct from '../LikedProduct/LikedProduct';
-import Policy from '../Policy';
 import About from '../About';
-import Consult from '../Consult';
-import Sale from '../Sale';
 import Account from '../Account';
+import Contact from '../Contact';
+import Home from '../Home';
+import Policy from '../Policy';
+import ProductDetail from '../ProductDetail';
+import ResultSearchOrder from '../ResultSearchOrder';
+import Sale from '../Sale';
+import Search from '../Search';
+import SearchOrder from '../SearchOrder';
 
 function HomePage() {
   return (
@@ -29,23 +20,14 @@ function HomePage() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sale" element={<Sale />} />
-      <Route path="/saleoff" element={<SaleOff />} />
       <Route path="/product" element={<ProductDetail brand="Rolex" type="Rolex bạc"/>} />
       <Route path="/search-order" element={<SearchOrder />} />
       <Route path="/result-search-order" element={<ResultSearchOrder />} />
-      <Route path="/news" element={<News />} />
-      <Route path="/watch-news/:id" element={<WatchNews />} />
       <Route path="/contact" element={<Contact />} />
-      <Route path="/cash" element={<Cash />} />
-      <Route path="/momo" element={<Momo />} />
       <Route path="/search" element={<Search />} />
-      <Route path="/payment" element={<Payment />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/liked-products" element={<LikedProduct />} />
       <Route path="/policy" element={<Policy />} />
       <Route path="/account/:id" element={<Account />} />
       <Route path="/about" element={<About />} />
-      <Route path="/consult" element={<Consult />} />
     </Routes>
     <Footer />
     </div>
