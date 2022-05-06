f318e280acb272c9788e5877f018524e59df33d3
TuanLeAnh1003 <nhoxtboy1000@gmail.com>
2022-05-06 22:43:36 +0700

    add env

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
 import mongoose from 'mongoose';
+import users from './router/users.js';
 
-import users from './router/users.js'
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const URI = 'mongodb+srv://leanhtuan:leanhtuan@cluster0.8svsf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const URI = process.env.DATABASE_URL
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '30mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors());
 
