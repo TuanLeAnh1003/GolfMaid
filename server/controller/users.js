49716f56d3ad4c500f4a5cb729ab61c8dad3c44f
TuanLeAnh1003 <nhoxtboy1000@gmail.com>
2022-05-09 19:10:34 +0700

    fix

--- server/controller/users.js
+++ server/controller/users.js
@@ -2,20 +2,20 @@ import { UserModel } from "../Models/UserModel.js"
 
 export const getUsers = async (req, res) => {
   // const user = new UserModel({
-  //   userId: 'U005',
-  //   password: '123456',
-  //   firstName: 'Công',
-  //   lastName: 'Chúa',
+  //   userId: "U100",
+  //   password: 'admin',
+  //   firstName: 'admin',
+  //   lastName: 'admin',
   //   birthday: new Date("1967-12-22"),
   //   phoneNumber: '0987654321',
-  //   email: 'congchuabongbong@gmail.com',
+  //   email: 'admin@gmail.com',
   //   address: {
   //     general: 'phường Linh Trung, thành phố Thủ Đức',
   //     detail: 'KTX A - ĐHQG HCM, khu phố 6, phường Linh Trung, thành phố Thủ Đức'
   //   },
-  //   role: 0,
+  //   role: 1,
   //   gender: 'male',
-  //   image: "https://via.placeholder.com/200"
+  //   image: "https://ipfs.infura.io/ipfs/QmZBZyho1Aw1gVGJCxFTgFaDiTb4PPNZsMa9iueZjNyd9C"
   // })
 
   // user.save();
